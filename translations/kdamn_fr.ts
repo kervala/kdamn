186104c0c8f930f34ffaae2b56efb2497a860a9b
Kervala <983521+kervala@users.noreply.github.com>
2013-07-21 14:31:16 +0200

    Changed: Updated translations

--- translations/kdamn_fr.ts
+++ translations/kdamn_fr.ts
@@ -1,115 +1,222 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="fr_FR">
+<TS version="2.0" language="fr_FR" sourcelanguage="en_US">
+<context>
+    <name>ChannelFrame</name>
+    <message>
+        <location filename="../src/channelframe.cpp" line="53"/>
+        <source>&lt;div class=&quot;title&quot;&gt;Topic is %1&lt;/div&gt;</source>
+        <translation>&lt;div class=&quot;title&quot;&gt;Le sujet est %1&lt;/div&gt;</translation>
+    </message>
+    <message>
+        <location filename="../src/channelframe.cpp" line="58"/>
+        <source>&lt;div class=&quot;title&quot;&gt;Title is %1&lt;/div&gt;</source>
+        <translation>&lt;div class=&quot;title&quot;&gt;Le titre est %1&lt;/div&gt;</translation>
+    </message>
+</context>
 <context>
     <name>ConnectDialog</name>
     <message>
-        <location filename="../ui/connectdialog.ui" line="17"/>
+        <location filename="../ui/connectdialog.ui" line="14"/>
         <source>Connect to server</source>
-        <translation type="unfinished"></translation>
+        <translation>Se connecter au serveur</translation>
     </message>
     <message>
-        <location filename="../ui/connectdialog.ui" line="26"/>
+        <location filename="../ui/connectdialog.ui" line="20"/>
         <source>Login or email</source>
-        <translation type="unfinished"></translation>
+        <translation>Identifiant ou email</translation>
     </message>
     <message>
-        <location filename="../ui/connectdialog.ui" line="36"/>
+        <location filename="../ui/connectdialog.ui" line="30"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Mot de passe</translation>
     </message>
     <message>
-        <location filename="../ui/connectdialog.ui" line="50"/>
+        <location filename="../ui/connectdialog.ui" line="44"/>
         <source>Authtoken</source>
-        <translation type="unfinished"></translation>
+        <translation>Authtoken</translation>
     </message>
     <message>
-        <location filename="../ui/connectdialog.ui" line="60"/>
+        <location filename="../ui/connectdialog.ui" line="54"/>
         <source>Remember password</source>
-        <translation type="unfinished"></translation>
+        <translation>Se souvenir du mot de passe</translation>
+    </message>
+</context>
+<context>
+    <name>DAmn</name>
+    <message>
+        <location filename="../src/damntablumps.cpp" line="106"/>
+        <source>Unable to parse tablump: %1</source>
+        <translation>Impossible de parser le tablump : %1</translation>
+    </message>
+    <message>
+        <location filename="../src/damntablumps.cpp" line="116"/>
+        <source>Tablump %1 not found in table</source>
+        <translation>Tablump %1 non trouvé dans la table</translation>
+    </message>
+    <message>
+        <location filename="../src/damntablumps.cpp" line="172"/>
+        <source>Image format %1 not recognized</source>
+        <translation>Format d&apos;images %1 non reconnu</translation>
+    </message>
+    <message>
+        <location filename="../src/damntablumps.cpp" line="353"/>
+        <source>Tablump %1 not recognized</source>
+        <translation>Tablump %1 non reconnu</translation>
+    </message>
+    <message>
+        <location filename="../src/damnparsers.cpp" line="38"/>
+        <source>Unable to recognize message: %1</source>
+        <translation>Impossible de déterminer le message: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/damnparsers.cpp" line="79"/>
+        <source>Unable to parse parameter: %1</source>
+        <translation>Impossinle de parser le paramètre : %1</translation>
+    </message>
+    <message>
+        <location filename="../src/damnparsers.cpp" line="110"/>
+        <source>Unable to parse property: %1</source>
+        <translation>Impossible de parser la propriété : %1</translation>
+    </message>
+    <message>
+        <location filename="../src/damnparsers.cpp" line="312"/>
+        <location filename="../src/damnparsers.cpp" line="452"/>
+        <source>Unable to recognize property: %1</source>
+        <translation>Impossible de déterminer la propriété : %1</translation>
+    </message>
+    <message>
+        <location filename="../src/damnparsers.cpp" line="382"/>
+        <source>Unable to parse privclass: %1</source>
+        <translation>Impossible de parser la privclass : %1</translation>
+    </message>
+    <message>
+        <location filename="../src/damn.cpp" line="173"/>
+        <source>Socket error: %1</source>
+        <translation>Erreur de socket : %1</translation>
+    </message>
+    <message>
+        <location filename="../src/damn.cpp" line="180"/>
+        <source>Network error: %1</source>
+        <translation>Erreur réseau : %1</translation>
+    </message>
+    <message>
+        <location filename="../src/damn.cpp" line="189"/>
+        <source>SSL errors: %1</source>
+        <translation>Erreur SSL : %1</translation>
+    </message>
+    <message>
+        <location filename="../src/damn.cpp" line="197"/>
+        <source>Authentication required</source>
+        <translation>Authentification requise</translation>
+    </message>
+    <message>
+        <location filename="../src/damn.cpp" line="299"/>
+        <source>Buffer too large (&gt; 8192)</source>
+        <translation>Buffer trop large (&gt; 8192)</translation>
     </message>
 </context>
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../ui/mainwindow.ui" line="57"/>
+        <location filename="../ui/mainwindow.ui" line="89"/>
         <source>&amp;Help</source>
         <translation>&amp;Aide</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="14"/>
-        <source>kdAmn</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../ui/mainwindow.ui" line="47"/>
+        <location filename="../ui/mainwindow.ui" line="79"/>
         <source>&amp;Server</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Serveur</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="65"/>
+        <location filename="../ui/mainwindow.ui" line="97"/>
         <source>Channel</source>
-        <translation type="unfinished"></translation>
+        <translation>Salon</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="93"/>
+        <location filename="../ui/mainwindow.ui" line="125"/>
         <source>&amp;About...</source>
         <translation>&amp;A propos...</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="101"/>
+        <location filename="../ui/mainwindow.ui" line="133"/>
         <source>About Qt</source>
         <translation>A propos de Qt</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="120"/>
+        <location filename="../ui/mainwindow.ui" line="152"/>
         <source>Connect</source>
-        <translation type="unfinished"></translation>
+        <translation>Connecter</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="125"/>
+        <location filename="../ui/mainwindow.ui" line="157"/>
         <source>Disconnect</source>
-        <translation type="unfinished"></translation>
+        <translation>Déconnecter</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="130"/>
+        <location filename="../ui/mainwindow.ui" line="162"/>
         <source>Join</source>
-        <translation type="unfinished"></translation>
+        <translation>Rejoindre</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="135"/>
+        <location filename="../ui/mainwindow.ui" line="167"/>
         <source>Part</source>
-        <translation type="unfinished"></translation>
+        <translation>Quitter</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="112"/>
+        <location filename="../ui/mainwindow.ui" line="144"/>
         <source>&amp;Logs</source>
         <translation>&amp;Journal</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="77"/>
+        <location filename="../ui/mainwindow.ui" line="37"/>
+        <source>Server</source>
+        <translation>Serveur</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="109"/>
         <source>&amp;Settings</source>
         <translation>&amp;Paramètres</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="85"/>
+        <location filename="../ui/mainwindow.ui" line="117"/>
         <source>E&amp;xit</source>
         <translation>&amp;Quitter</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="75"/>
+        <location filename="../src/mainwindow.cpp" line="64"/>
+        <source>Restore</source>
+        <translation>Restaurer</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="66"/>
+        <source>Quit</source>
+        <translation>Quitter</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="89"/>
         <source>About %1 %2 by %3</source>
         <translation>A propos de %1 %2 par %3</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="143"/>
+        <location filename="../src/mainwindow.cpp" line="134"/>
         <source>Join channel</source>
-        <translation type="unfinished"></translation>
+        <translation>Rejoindre le salon</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="143"/>
+        <location filename="../src/mainwindow.cpp" line="134"/>
         <source>Please enter channel to join</source>
-        <translation type="unfinished"></translation>
+        <translation>Veuillez entrer le salon à rejoindre</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="147"/>
+        <source>Connected to server</source>
+        <translation>Connecté au serveur</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="196"/>
+        <source>You joined channel &lt;b&gt;%1&lt;/b&gt;</source>
+        <translation>Vous avez rejoint le salon &lt;b&gt;%1&lt;/b&gt;</translation>
     </message>
 </context>
 </TS>
