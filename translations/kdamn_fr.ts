05348e857e5547f4c97c7c0476116918b5fcb3ea
Kervala <983521+kervala@users.noreply.github.com>
2013-07-27 16:45:29 +0200

    Changed: Update translations

--- translations/kdamn_fr.ts
+++ translations/kdamn_fr.ts
@@ -4,14 +4,14 @@
 <context>
     <name>ChannelFrame</name>
     <message>
-        <location filename="../src/channelframe.cpp" line="53"/>
-        <source>&lt;div class=&quot;title&quot;&gt;Topic is %1&lt;/div&gt;</source>
-        <translation>&lt;div class=&quot;title&quot;&gt;Le sujet est %1&lt;/div&gt;</translation>
+        <location filename="../src/channelframe.cpp" line="85"/>
+        <source>%1 has joined</source>
+        <translation>%1 a rejoint le salon</translation>
     </message>
     <message>
-        <location filename="../src/channelframe.cpp" line="58"/>
-        <source>&lt;div class=&quot;title&quot;&gt;Title is %1&lt;/div&gt;</source>
-        <translation>&lt;div class=&quot;title&quot;&gt;Le titre est %1&lt;/div&gt;</translation>
+        <location filename="../src/channelframe.cpp" line="99"/>
+        <source>%1 has left</source>
+        <translation>%1 a quitté le salon</translation>
     </message>
 </context>
 <context>
@@ -32,12 +32,46 @@
         <translation>Mot de passe</translation>
     </message>
     <message>
-        <location filename="../ui/connectdialog.ui" line="44"/>
-        <source>Authtoken</source>
-        <translation>Authtoken</translation>
+        <location filename="../ui/connectdialog.ui" line="51"/>
+        <source>dAmn token</source>
+        <translation>Jeton dAmn</translation>
     </message>
     <message>
-        <location filename="../ui/connectdialog.ui" line="54"/>
+        <location filename="../ui/connectdialog.ui" line="61"/>
+        <source>Method</source>
+        <translation>Méthode</translation>
+    </message>
+    <message>
+        <location filename="../ui/connectdialog.ui" line="68"/>
+        <source>kdAmn will be automatically authorized for
+your account and you&apos;ll be able to use all
+ functionnalities such as Stash upload.</source>
+        <translation>kdAmn sera automatiquement autorisé
+ pour votre compte et vous serez capable
+d&apos;utiliser toutes les fonctionnalités comme
+l&apos;upload sur Stash.</translation>
+    </message>
+    <message>
+        <location filename="../ui/connectdialog.ui" line="73"/>
+        <source>OAuth2 deviantART API (preferred)</source>
+        <translation>API deviantART OAuth2 (préférée)</translation>
+    </message>
+    <message>
+        <location filename="../ui/connectdialog.ui" line="83"/>
+        <source>kdAmn will connect to deviantART website
+like a browser and get the dAmn token from HTML.
+Stash upload won&apos;t be available.</source>
+        <translation>kdAmn se connectera au site deviantART comme un
+navigateur et récupérera le jeton dAmn du code HTML.
+L&apos;upload sur Stash ne sera pas possible.</translation>
+    </message>
+    <message>
+        <location filename="../ui/connectdialog.ui" line="88"/>
+        <source>deviantART website login</source>
+        <translation>Login au site deviantART</translation>
+    </message>
+    <message>
+        <location filename="../ui/connectdialog.ui" line="44"/>
         <source>Remember password</source>
         <translation>Se souvenir du mot de passe</translation>
     </message>
@@ -45,75 +79,93 @@
 <context>
     <name>DAmn</name>
     <message>
-        <location filename="../src/damntablumps.cpp" line="106"/>
+        <location filename="../src/damntablumps.cpp" line="101"/>
         <source>Unable to parse tablump: %1</source>
         <translation>Impossible de parser le tablump : %1</translation>
     </message>
     <message>
-        <location filename="../src/damntablumps.cpp" line="116"/>
+        <location filename="../src/damntablumps.cpp" line="111"/>
         <source>Tablump %1 not found in table</source>
         <translation>Tablump %1 non trouvé dans la table</translation>
     </message>
     <message>
-        <location filename="../src/damntablumps.cpp" line="172"/>
-        <source>Image format %1 not recognized</source>
-        <translation>Format d&apos;images %1 non reconnu</translation>
-    </message>
-    <message>
-        <location filename="../src/damntablumps.cpp" line="353"/>
+        <location filename="../src/damntablumps.cpp" line="292"/>
         <source>Tablump %1 not recognized</source>
         <translation>Tablump %1 non reconnu</translation>
     </message>
     <message>
-        <location filename="../src/damnparsers.cpp" line="38"/>
+        <location filename="../src/damnparsers.cpp" line="51"/>
         <source>Unable to recognize message: %1</source>
         <translation>Impossible de déterminer le message: %1</translation>
     </message>
     <message>
-        <location filename="../src/damnparsers.cpp" line="79"/>
+        <location filename="../src/damnparsers.cpp" line="96"/>
         <source>Unable to parse parameter: %1</source>
         <translation>Impossinle de parser le paramètre : %1</translation>
     </message>
     <message>
-        <location filename="../src/damnparsers.cpp" line="110"/>
+        <location filename="../src/damnparsers.cpp" line="129"/>
         <source>Unable to parse property: %1</source>
         <translation>Impossible de parser la propriété : %1</translation>
     </message>
     <message>
-        <location filename="../src/damnparsers.cpp" line="312"/>
-        <location filename="../src/damnparsers.cpp" line="452"/>
+        <location filename="../src/damnparsers.cpp" line="159"/>
+        <source>Bad parameter</source>
+        <translation>Mauvais paramètre</translation>
+    </message>
+    <message>
+        <location filename="../src/damnparsers.cpp" line="160"/>
+        <source>Unknown property</source>
+        <translation>Propriété inconnue</translation>
+    </message>
+    <message>
+        <location filename="../src/damnparsers.cpp" line="161"/>
+        <source>Authentication failed</source>
+        <translation>Echec de l&apos;authentification</translation>
+    </message>
+    <message>
+        <location filename="../src/damnparsers.cpp" line="162"/>
+        <source>Nothing to send</source>
+        <translation>Rien à envoyer</translation>
+    </message>
+    <message>
+        <location filename="../src/damnparsers.cpp" line="176"/>
+        <source>Error &quot;%1&quot; not recognized</source>
+        <translation>Error &quot;%1&quot; non reconnue</translation>
+    </message>
+    <message>
+        <location filename="../src/damnparsers.cpp" line="337"/>
+        <source>Unable to recognize param %1</source>
+        <translation>Impossible de reconnaître le paramètre %1</translation>
+    </message>
+    <message>
+        <location filename="../src/damnparsers.cpp" line="412"/>
+        <location filename="../src/damnparsers.cpp" line="559"/>
         <source>Unable to recognize property: %1</source>
         <translation>Impossible de déterminer la propriété : %1</translation>
     </message>
     <message>
-        <location filename="../src/damnparsers.cpp" line="382"/>
+        <location filename="../src/damnparsers.cpp" line="487"/>
         <source>Unable to parse privclass: %1</source>
         <translation>Impossible de parser la privclass : %1</translation>
     </message>
     <message>
-        <location filename="../src/damn.cpp" line="173"/>
+        <location filename="../src/damn.cpp" line="82"/>
         <source>Socket error: %1</source>
         <translation>Erreur de socket : %1</translation>
     </message>
     <message>
-        <location filename="../src/damn.cpp" line="180"/>
-        <source>Network error: %1</source>
-        <translation>Erreur réseau : %1</translation>
-    </message>
-    <message>
-        <location filename="../src/damn.cpp" line="189"/>
-        <source>SSL errors: %1</source>
-        <translation>Erreur SSL : %1</translation>
-    </message>
-    <message>
-        <location filename="../src/damn.cpp" line="197"/>
-        <source>Authentication required</source>
-        <translation>Authentification requise</translation>
-    </message>
-    <message>
-        <location filename="../src/damn.cpp" line="299"/>
+        <location filename="../src/damn.cpp" line="150"/>
         <source>Buffer too large (&gt; 8192)</source>
         <translation>Buffer trop large (&gt; 8192)</translation>
+    </message>
+</context>
+<context>
+    <name>InputEdit</name>
+    <message>
+        <location filename="../src/inputedit.cpp" line="106"/>
+        <source>History</source>
+        <translation>Historique</translation>
     </message>
 </context>
 <context>
@@ -184,39 +236,72 @@
         <translation>&amp;Quitter</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="64"/>
-        <source>Restore</source>
-        <translation>Restaurer</translation>
-    </message>
-    <message>
-        <location filename="../src/mainwindow.cpp" line="66"/>
-        <source>Quit</source>
-        <translation>Quitter</translation>
-    </message>
-    <message>
-        <location filename="../src/mainwindow.cpp" line="89"/>
+        <location filename="../src/mainwindow.cpp" line="101"/>
         <source>About %1 %2 by %3</source>
         <translation>A propos de %1 %2 par %3</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="134"/>
+        <location filename="../src/mainwindow.cpp" line="156"/>
         <source>Join channel</source>
         <translation>Rejoindre le salon</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="134"/>
+        <location filename="../src/mainwindow.cpp" line="156"/>
         <source>Please enter channel to join</source>
         <translation>Veuillez entrer le salon à rejoindre</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="147"/>
+        <location filename="../src/mainwindow.cpp" line="182"/>
         <source>Connected to server</source>
         <translation>Connecté au serveur</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="196"/>
+        <location filename="../src/mainwindow.cpp" line="211"/>
+        <source>Topic changed by %1: %2</source>
+        <translation>Sujet modifié par %1 : %2</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="212"/>
+        <source>Title changed by %1: %2</source>
+        <translation>Titre modifié par %1 : %2</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="246"/>
+        <source>%1 has been made a member of %2 by %3</source>
+        <translation>%1 a été promu comme membre de %2 par %3</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="260"/>
         <source>You joined channel &lt;b&gt;%1&lt;/b&gt;</source>
         <translation>Vous avez rejoint le salon &lt;b&gt;%1&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="267"/>
+        <source>You leaved channel &lt;b&gt;%1&lt;/b&gt;</source>
+        <translation>Vous avez quitté le salon &lt;b&gt;%1&lt;/b&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>OAuth2</name>
+    <message>
+        <location filename="../src/oauth2.cpp" line="335"/>
+        <source>No JSON status received</source>
+        <translation>Aucun status JSON reçu</translation>
+    </message>
+    <message>
+        <location filename="../src/oauth2.cpp" line="399"/>
+        <source>Network error: %1</source>
+        <translation>Erreur réseau : %1</translation>
+    </message>
+    <message>
+        <location filename="../src/oauth2.cpp" line="408"/>
+        <source>SSL errors: %1</source>
+        <translation>Erreur SSL : %1</translation>
+    </message>
+    <message>
+        <location filename="../src/oauth2.cpp" line="416"/>
+        <source>Authentication required</source>
+        <translation>Authentification requise</translation>
     </message>
 </context>
 </TS>
