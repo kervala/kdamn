10b2f7d5194c4a00d506b45ddecde2f302ae1015
Kervala <983521+kervala@users.noreply.github.com>
2013-08-04 19:24:47 +0200

    Changed: Updated french translations

--- translations/kdamn_fr.ts
+++ translations/kdamn_fr.ts
@@ -2,19 +2,6 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="fr_FR" sourcelanguage="en_US">
 <context>
-    <name>ChannelFrame</name>
-    <message>
-        <location filename="../src/channelframe.cpp" line="85"/>
-        <source>%1 has joined</source>
-        <translation>%1 a rejoint le salon</translation>
-    </message>
-    <message>
-        <location filename="../src/channelframe.cpp" line="99"/>
-        <source>%1 has left</source>
-        <translation>%1 a quitté le salon</translation>
-    </message>
-</context>
-<context>
     <name>ConnectDialog</name>
     <message>
         <location filename="../ui/connectdialog.ui" line="14"/>
@@ -27,22 +14,22 @@
         <translation>Identifiant ou email</translation>
     </message>
     <message>
-        <location filename="../ui/connectdialog.ui" line="30"/>
+        <location filename="../ui/connectdialog.ui" line="33"/>
         <source>Password</source>
         <translation>Mot de passe</translation>
     </message>
     <message>
-        <location filename="../ui/connectdialog.ui" line="51"/>
+        <location filename="../ui/connectdialog.ui" line="57"/>
         <source>dAmn token</source>
         <translation>Jeton dAmn</translation>
     </message>
     <message>
-        <location filename="../ui/connectdialog.ui" line="61"/>
+        <location filename="../ui/connectdialog.ui" line="70"/>
         <source>Method</source>
         <translation>Méthode</translation>
     </message>
     <message>
-        <location filename="../ui/connectdialog.ui" line="68"/>
+        <location filename="../ui/connectdialog.ui" line="80"/>
         <source>kdAmn will be automatically authorized for
 your account and you&apos;ll be able to use all
  functionnalities such as Stash upload.</source>
@@ -52,12 +39,12 @@ d&apos;utiliser toutes les fonctionnalités comme
 l&apos;upload sur Stash.</translation>
     </message>
     <message>
-        <location filename="../ui/connectdialog.ui" line="73"/>
+        <location filename="../ui/connectdialog.ui" line="85"/>
         <source>OAuth2 deviantART API (preferred)</source>
         <translation>API deviantART OAuth2 (préférée)</translation>
     </message>
     <message>
-        <location filename="../ui/connectdialog.ui" line="83"/>
+        <location filename="../ui/connectdialog.ui" line="95"/>
         <source>kdAmn will connect to deviantART website
 like a browser and get the dAmn token from HTML.
 Stash upload won&apos;t be available.</source>
@@ -66,12 +53,12 @@ navigateur et récupérera le jeton dAmn du code HTML.
 L&apos;upload sur Stash ne sera pas possible.</translation>
     </message>
     <message>
-        <location filename="../ui/connectdialog.ui" line="88"/>
+        <location filename="../ui/connectdialog.ui" line="100"/>
         <source>deviantART website login</source>
         <translation>Login au site deviantART</translation>
     </message>
     <message>
-        <location filename="../ui/connectdialog.ui" line="44"/>
+        <location filename="../ui/connectdialog.ui" line="50"/>
         <source>Remember password</source>
         <translation>Se souvenir du mot de passe</translation>
     </message>
@@ -79,17 +66,17 @@ L&apos;upload sur Stash ne sera pas possible.</translation>
 <context>
     <name>DAmn</name>
     <message>
-        <location filename="../src/damntablumps.cpp" line="101"/>
+        <location filename="../src/damntablumps.cpp" line="102"/>
         <source>Unable to parse tablump: %1</source>
         <translation>Impossible de parser le tablump : %1</translation>
     </message>
     <message>
-        <location filename="../src/damntablumps.cpp" line="111"/>
+        <location filename="../src/damntablumps.cpp" line="112"/>
         <source>Tablump %1 not found in table</source>
         <translation>Tablump %1 non trouvé dans la table</translation>
     </message>
     <message>
-        <location filename="../src/damntablumps.cpp" line="292"/>
+        <location filename="../src/damntablumps.cpp" line="302"/>
         <source>Tablump %1 not recognized</source>
         <translation>Tablump %1 non reconnu</translation>
     </message>
@@ -129,23 +116,33 @@ L&apos;upload sur Stash ne sera pas possible.</translation>
         <translation>Rien à envoyer</translation>
     </message>
     <message>
-        <location filename="../src/damnparsers.cpp" line="176"/>
+        <location filename="../src/damnparsers.cpp" line="163"/>
+        <source>Already joined</source>
+        <translation>Déjà rejoint</translation>
+    </message>
+    <message>
+        <location filename="../src/damnparsers.cpp" line="177"/>
         <source>Error &quot;%1&quot; not recognized</source>
         <translation>Error &quot;%1&quot; non reconnue</translation>
     </message>
     <message>
-        <location filename="../src/damnparsers.cpp" line="337"/>
+        <location filename="../src/damnparsers.cpp" line="338"/>
         <source>Unable to recognize param %1</source>
         <translation>Impossible de reconnaître le paramètre %1</translation>
     </message>
     <message>
-        <location filename="../src/damnparsers.cpp" line="412"/>
-        <location filename="../src/damnparsers.cpp" line="559"/>
+        <location filename="../src/damnparsers.cpp" line="374"/>
+        <source>Private chats not yet implemented</source>
+        <translation>Chats privés pas encore implémentés</translation>
+    </message>
+    <message>
+        <location filename="../src/damnparsers.cpp" line="420"/>
+        <location filename="../src/damnparsers.cpp" line="565"/>
         <source>Unable to recognize property: %1</source>
         <translation>Impossible de déterminer la propriété : %1</translation>
     </message>
     <message>
-        <location filename="../src/damnparsers.cpp" line="487"/>
+        <location filename="../src/damnparsers.cpp" line="495"/>
         <source>Unable to parse privclass: %1</source>
         <translation>Impossible de parser la privclass : %1</translation>
     </message>
@@ -155,7 +152,7 @@ L&apos;upload sur Stash ne sera pas possible.</translation>
         <translation>Erreur de socket : %1</translation>
     </message>
     <message>
-        <location filename="../src/damn.cpp" line="150"/>
+        <location filename="../src/damn.cpp" line="201"/>
         <source>Buffer too large (&gt; 8192)</source>
         <translation>Buffer trop large (&gt; 8192)</translation>
     </message>
@@ -163,145 +160,241 @@ L&apos;upload sur Stash ne sera pas possible.</translation>
 <context>
     <name>InputEdit</name>
     <message>
-        <location filename="../src/inputedit.cpp" line="106"/>
+        <location filename="../src/inputedit.cpp" line="128"/>
         <source>History</source>
         <translation>Historique</translation>
+    </message>
+    <message>
+        <location filename="../src/inputedit.cpp" line="150"/>
+        <source>Clear history</source>
+        <translation>Effacer l&apos;historique</translation>
+    </message>
+</context>
+<context>
+    <name>JoinRoomDialog</name>
+    <message>
+        <location filename="../ui/joinroomdialog.ui" line="14"/>
+        <source>Join room</source>
+        <translation>Rejoindre le salon</translation>
+    </message>
+    <message>
+        <location filename="../ui/joinroomdialog.ui" line="20"/>
+        <source>Please select or enter room to join</source>
+        <translation>Veuillez sélectionner ou entrer le salon à rejoindre</translation>
     </message>
 </context>
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../ui/mainwindow.ui" line="89"/>
+        <location filename="../ui/mainwindow.ui" line="60"/>
         <source>&amp;Help</source>
         <translation>&amp;Aide</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="79"/>
+        <location filename="../ui/mainwindow.ui" line="50"/>
         <source>&amp;Server</source>
         <translation>&amp;Serveur</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="97"/>
-        <source>Channel</source>
+        <location filename="../ui/mainwindow.ui" line="68"/>
+        <source>Room</source>
         <translation>Salon</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="125"/>
+        <location filename="../ui/mainwindow.ui" line="96"/>
         <source>&amp;About...</source>
         <translation>&amp;A propos...</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="133"/>
+        <location filename="../ui/mainwindow.ui" line="104"/>
         <source>About Qt</source>
         <translation>A propos de Qt</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="152"/>
+        <location filename="../ui/mainwindow.ui" line="123"/>
         <source>Connect</source>
         <translation>Connecter</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="157"/>
+        <location filename="../ui/mainwindow.ui" line="128"/>
         <source>Disconnect</source>
         <translation>Déconnecter</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="162"/>
+        <location filename="../ui/mainwindow.ui" line="133"/>
         <source>Join</source>
         <translation>Rejoindre</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="167"/>
+        <location filename="../ui/mainwindow.ui" line="138"/>
         <source>Part</source>
         <translation>Quitter</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="144"/>
+        <location filename="../ui/mainwindow.ui" line="143"/>
+        <location filename="../ui/mainwindow.ui" line="146"/>
+        <source>Known rooms</source>
+        <translation>Salons connus</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="115"/>
         <source>&amp;Logs</source>
         <translation>&amp;Journal</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="37"/>
         <source>Server</source>
-        <translation>Serveur</translation>
+        <translation type="obsolete">Serveur</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="109"/>
+        <location filename="../ui/mainwindow.ui" line="80"/>
         <source>&amp;Settings</source>
         <translation>&amp;Paramètres</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="117"/>
+        <location filename="../ui/mainwindow.ui" line="88"/>
         <source>E&amp;xit</source>
         <translation>&amp;Quitter</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="101"/>
+        <location filename="../src/mainwindow.cpp" line="77"/>
         <source>About %1 %2 by %3</source>
         <translation>A propos de %1 %2 par %3</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="156"/>
-        <source>Join channel</source>
-        <translation>Rejoindre le salon</translation>
+        <location filename="../src/mainwindow.cpp" line="78"/>
+        <source>deviantART SVG icon by &lt;a href=&quot;http://abluescarab.deviantart.com&quot;&gt;Alana Gilston&lt;/a&gt;</source>
+        <translation>Icône SVG de deviantART par &lt;a href=&quot;http://abluescarab.deviantart.com&quot;&gt;Alana Gilston&lt;/a&gt;</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="156"/>
-        <source>Please enter channel to join</source>
-        <translation>Veuillez entrer le salon à rejoindre</translation>
+        <source>Join room</source>
+        <translation type="obsolete">Rejoindre le salon</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="182"/>
+        <source>Please enter room to join</source>
+        <translation type="obsolete">Veuillez entrer le salon à rejoindre</translation>
+    </message>
+    <message>
         <source>Connected to server</source>
-        <translation>Connecté au serveur</translation>
+        <translation type="obsolete">Connecté au serveur</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="211"/>
         <source>Topic changed by %1: %2</source>
-        <translation>Sujet modifié par %1 : %2</translation>
+        <translation type="obsolete">Sujet modifié par %1 : %2</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="212"/>
         <source>Title changed by %1: %2</source>
-        <translation>Titre modifié par %1 : %2</translation>
+        <translation type="obsolete">Titre modifié par %1 : %2</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="246"/>
         <source>%1 has been made a member of %2 by %3</source>
-        <translation>%1 a été promu comme membre de %2 par %3</translation>
+        <translation type="obsolete">%1 a été promu comme membre de %2 par %3</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="260"/>
-        <source>You joined channel &lt;b&gt;%1&lt;/b&gt;</source>
-        <translation>Vous avez rejoint le salon &lt;b&gt;%1&lt;/b&gt;</translation>
+        <source>You joined room &lt;b&gt;%1&lt;/b&gt;</source>
+        <translation type="obsolete">Vous avez rejoint le salon &lt;b&gt;%1&lt;/b&gt;</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="267"/>
-        <source>You leaved channel &lt;b&gt;%1&lt;/b&gt;</source>
-        <translation>Vous avez quitté le salon &lt;b&gt;%1&lt;/b&gt;</translation>
+        <source>You leaved room &lt;b&gt;%1&lt;/b&gt;</source>
+        <translation type="obsolete">Vous avez quitté le salon &lt;b&gt;%1&lt;/b&gt;</translation>
     </message>
 </context>
 <context>
     <name>OAuth2</name>
     <message>
-        <location filename="../src/oauth2.cpp" line="335"/>
+        <location filename="../src/oauth2.cpp" line="313"/>
+        <location filename="../src/oauth2.cpp" line="332"/>
+        <source>No login received</source>
+        <translation>Aucun identifiant reçu</translation>
+    </message>
+    <message>
+        <location filename="../src/oauth2.cpp" line="358"/>
+        <location filename="../src/oauth2.cpp" line="377"/>
+        <source>No dAmn token received</source>
+        <translation>Aucun jeton dAmn reçu</translation>
+    </message>
+    <message>
+        <location filename="../src/oauth2.cpp" line="424"/>
         <source>No JSON status received</source>
         <translation>Aucun status JSON reçu</translation>
     </message>
     <message>
-        <location filename="../src/oauth2.cpp" line="399"/>
+        <location filename="../src/oauth2.cpp" line="515"/>
         <source>Network error: %1</source>
         <translation>Erreur réseau : %1</translation>
     </message>
     <message>
-        <location filename="../src/oauth2.cpp" line="408"/>
+        <location filename="../src/oauth2.cpp" line="524"/>
         <source>SSL errors: %1</source>
         <translation>Erreur SSL : %1</translation>
     </message>
     <message>
-        <location filename="../src/oauth2.cpp" line="416"/>
+        <location filename="../src/oauth2.cpp" line="532"/>
         <source>Authentication required</source>
         <translation>Authentification requise</translation>
+    </message>
+</context>
+<context>
+    <name>RoomFrame</name>
+    <message>
+        <location filename="../src/roomframe.cpp" line="84"/>
+        <source>%1 has joined</source>
+        <translation>%1 a rejoint le salon</translation>
+    </message>
+    <message>
+        <location filename="../src/roomframe.cpp" line="98"/>
+        <source>%1 has left</source>
+        <translation>%1 a quitté le salon</translation>
+    </message>
+</context>
+<context>
+    <name>RoomsDialog</name>
+    <message>
+        <location filename="../ui/roomsdialog.ui" line="14"/>
+        <source>Rooms</source>
+        <translation>Salons</translation>
+    </message>
+    <message>
+        <location filename="../ui/roomsdialog.ui" line="20"/>
+        <source>Check rooms you want to join automatically at startup</source>
+        <translation>Cocher les salons que vous souhaitez rejoindre automatiquement au démarrage</translation>
+    </message>
+</context>
+<context>
+    <name>RoomsTabWidget</name>
+    <message>
+        <location filename="../src/roomstabwidget.cpp" line="63"/>
+        <source>Server</source>
+        <translation>Serveur</translation>
+    </message>
+    <message>
+        <location filename="../src/roomstabwidget.cpp" line="123"/>
+        <source>Connected to server</source>
+        <translation>Connecté au serveur</translation>
+    </message>
+    <message>
+        <location filename="../src/roomstabwidget.cpp" line="187"/>
+        <source>Topic changed by %1: %2</source>
+        <translation>Sujet modifié par %1 : %2</translation>
+    </message>
+    <message>
+        <location filename="../src/roomstabwidget.cpp" line="188"/>
+        <source>Title changed by %1: %2</source>
+        <translation>Titre modifié par %1 : %2</translation>
+    </message>
+    <message>
+        <location filename="../src/roomstabwidget.cpp" line="228"/>
+        <source>%1 has been made a member of %2 by %3</source>
+        <translation>%1 a été promu comme membre de %2 par %3</translation>
+    </message>
+    <message>
+        <location filename="../src/roomstabwidget.cpp" line="242"/>
+        <source>You joined room &lt;b&gt;%1&lt;/b&gt;</source>
+        <translation>Vous avez rejoint le salon &lt;b&gt;%1&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="../src/roomstabwidget.cpp" line="251"/>
+        <source>You leaved room &lt;b&gt;%1&lt;/b&gt;</source>
+        <translation>Vous avez quitté le salon &lt;b&gt;%1&lt;/b&gt;</translation>
     </message>
 </context>
 </TS>
