db96a132954eaee8bb1dfdae70a176cc70e1c71d
Kervala <983521+kervala@users.noreply.github.com>
2013-12-15 16:45:37 +0100

    Changed: Memory leaks detection before Qt headers

--- src/common.h
+++ src/common.h
@@ -20,6 +20,14 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#if defined(_MSC_VER) && defined(_DEBUG)
+	#define _CRTDBG_MAP_ALLOC
+	#include <stdlib.h>
+	#include <crtdbg.h>
+	#define DEBUG_NEW new(_NORMAL_BLOCK, __FILE__, __LINE__)
+	#undef realloc
+#endif
+
 #include <QtCore/QtCore>
 #include <QtGui/QtGui>
 #include <QtNetwork/QtNetwork>
@@ -34,13 +42,6 @@
 #include <QtConcurrent/QtConcurrent>
 #else
 #include <QtScript/QtScript>
-#endif
-
-#if defined(_MSC_VER) && defined(_DEBUG)
-	#define _CRTDBG_MAP_ALLOC
-	#include <stdlib.h>
-	#include <crtdbg.h>
-	#define DEBUG_NEW new(_NORMAL_BLOCK, __FILE__, __LINE__)
 #endif
 
 #endif
