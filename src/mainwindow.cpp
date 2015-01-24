c0ca409568172e229c91d49780990c36dd67e73e
Kervala <983521+kervala@users.noreply.github.com>
2015-01-24 19:42:02 +0100

    Changed: No need to credit old logo artist becure using the official one

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -155,8 +155,7 @@ void MainWindow::onCheckUpdates()
 
 void MainWindow::onAbout()
 {
-	QMessageBox::about(this, tr("About %1 %2 by %3").arg(PRODUCT).arg(VERSION).arg(AUTHOR),
-		QString("%1<br><br>%2").arg(tr(DESCRIPTION)).arg(tr("deviantART SVG icon by <a href=\"http://abluescarab.deviantart.com\">Alana Gilston</a>")));
+	QMessageBox::about(this, tr("About %1 %2 by %3").arg(PRODUCT).arg(VERSION).arg(AUTHOR), DESCRIPTION);
 }
 
 void MainWindow::onAboutQt()
