76c329da26b69ed4e6b93e943b6eecd95a08adb6
Kervala <983521+kervala@users.noreply.github.com>
2015-02-07 18:25:08 +0100

    Fixed: Compilation under Linux

--- CMakeModules/QtSupport.cmake
+++ CMakeModules/QtSupport.cmake
@@ -95,7 +95,12 @@ MACRO(USE_QT_MODULES)
   SET(QT4_MODULES ${QT_SHARED_MODULES} Main)
 
   # Qt 5 modules
-  SET(QT5_MODULES ${QT_SHARED_MODULES} Bluetooth Concurrent LinguistTools MultimediaQuick MultimediaWidgets Nfc OpenGL OpenGLExtensions PlatformSupport Positioning PrintSupport Quick QuickParticles QuickTest QuickWidgets Sensors SerialPort V8 WebChannel WebKitWidgets WebSockets Widgets WinExtras)
+  SET(QT5_MODULES ${QT_SHARED_MODULES} Bluetooth Concurrent LinguistTools MultimediaQuick MultimediaWidgets Nfc OpenGL OpenGLExtensions PlatformSupport Positioning PrintSupport Quick QuickParticles QuickTest QuickWidgets Sensors SerialPort V8 WebChannel WebKitWidgets WebSockets Widgets)
+  
+  # Use WinExtras only under Windows
+  IF(WIN32)
+    LIST(APPEND QT5_MODULES WinExtras)
+  ENDIF()
 
   IF(WITH_QT5)
     # Look for Qt 5 in some environment variables
