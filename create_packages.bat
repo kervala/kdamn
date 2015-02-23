cf473f095b193a7f542fb1ca8e9c557d28e2cecf
Kervala <983521+kervala@users.noreply.github.com>
2015-02-23 18:44:12 +0100

    Changed: Build 64 bits packages first

--- create_packages.bat
+++ create_packages.bat
@@ -1,14 +1,14 @@
 set UPLOADER="C:\Program Files\RedmineUploader\redmineuploader.exe"
 
-call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x86
+call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
 
-set QTDIR=D:/External/vc10/qt-5.4.0-static-32
+set QTDIR=D:/External/vc10/qt-5.4.0-static-64
 
-rmdir /s /q package_x32
+rmdir /s /q package_x64
 
-mkdir package_x32
+mkdir package_x64
 
-cd package_x32
+cd package_x64
 cmake .. -DCMAKE_BUILD_TYPE=Release -G"NMake Makefiles"
 
 nmake package
@@ -19,15 +19,15 @@ cd ..
 
 rem pause
 
-call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
+call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x86
 
-set QTDIR=D:/External/vc10/qt-5.4.0-static-64
+set QTDIR=D:/External/vc10/qt-5.4.0-static-32
 
-rmdir /s /q package_x64
+rmdir /s /q package_x32
 
-mkdir package_x64
+mkdir package_x32
 
-cd package_x64
+cd package_x32
 cmake .. -DCMAKE_BUILD_TYPE=Release -G"NMake Makefiles"
 
 nmake package
