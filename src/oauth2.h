feb907b7528f9a3c2f3923abc08377d5a73fe35b
Kervala <983521+kervala@users.noreply.github.com>
2017-01-18 17:35:42 +0100

    Fixed: Compilation with clang

--- src/oauth2.h
+++ src/oauth2.h
@@ -24,21 +24,21 @@
 
 // deviantART URLs
 #define BASE_URL "www.deviantart.com"
-#define HTTPS_URL "https://"BASE_URL
-#define HTTP_URL "http://"BASE_URL
-#define LOGIN_URL HTTPS_URL"/users/login"
-#define OAUTH2LOGIN_URL HTTPS_URL"/join/oauth2login"
-#define LOGOUT_URL HTTPS_URL"/settings/force-logout"
-#define ROCKEDOUT_URL HTTPS_URL"/users/rockedout"
-#define NOTES_URL HTTPS_URL"/messages/notes/#1_0"
-#define SENDNOTE_URL HTTP_URL"/messages/notes/send"
-#define OAUTH2_URL HTTPS_URL"/api/v1/oauth2"
-#define OAUTH2_TOKEN_URL HTTPS_URL"/oauth2/token"
-#define DIFI_URL HTTPS_URL"/global/difi.php"
+#define HTTPS_URL "https://" BASE_URL
+#define HTTP_URL "http://" BASE_URL
+#define LOGIN_URL HTTPS_URL "/users/login"
+#define OAUTH2LOGIN_URL HTTPS_URL "/join/oauth2login"
+#define LOGOUT_URL HTTPS_URL "/settings/force-logout"
+#define ROCKEDOUT_URL HTTPS_URL "/users/rockedout"
+#define NOTES_URL HTTPS_URL "/messages/notes/#1_0"
+#define SENDNOTE_URL HTTP_URL "/messages/notes/send"
+#define OAUTH2_URL HTTPS_URL "/api/v1/oauth2"
+#define OAUTH2_TOKEN_URL HTTPS_URL "/oauth2/token"
+#define DIFI_URL HTTPS_URL "/global/difi.php"
 #define REDIRECT_APP "kdamn://oauth2/login"
-#define AUTHORIZE_URL HTTPS_URL"/oauth2/authorize"
-#define JOIN_URL HTTPS_URL"/join/oauth2"
-#define SESSIONS_URL HTTPS_URL"/settings/sessions"
+#define AUTHORIZE_URL HTTPS_URL "/oauth2/authorize"
+#define JOIN_URL HTTPS_URL "/join/oauth2"
+#define SESSIONS_URL HTTPS_URL "/settings/sessions"
 
 struct NoteForm
 {
