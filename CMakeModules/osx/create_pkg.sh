3f5ee2b36ce04310a7ff63a9fef683d169c3094a
Kervala <983521+kervala@users.noreply.github.com>
2017-12-09 18:36:49 +0100

    Fixed: Update packages scripts in .sh

--- CMakeModules/osx/create_pkg.sh
+++ CMakeModules/osx/create_pkg.sh
@@ -8,6 +8,8 @@
 #    |- PackageInfo
 #    |- Payload
 
+# Uncompress Payload : cpio -iv < Payload
+
 PKGDIR=$1
 APPDIR=$2
 PKGFILE=$3
@@ -26,7 +28,6 @@ fi
 
 PACKAGEDIR=$(ls -1 -d $PKGDIR/*.pkg)
 
-echo "PACKAGEDIR $PACKAGEDIR"
 # TODO: check only one line
 
 if [ ! -e "$PACKAGEDIR/PackageInfo" ]
@@ -34,6 +35,14 @@ then
   echo "Unable to find PackageInfo file"
   exit 1
 fi
+
+# variables to change in files
+NUMBER_OF_FILES=$(find $APPDIR | wc -l)
+INSTALL_KBYTES=$(du -b -s $APPDIR)
+
+sed -i "s/NUMBER_OF_FILES/$NUMBER_OF_FILES/g" $DIR/Distribution
+sed -i "s/INSTALL_KBYTES/$INSTALL_KBYTES/g" $DIR/Distribution
+sed -i "s/INSTALL_KBYTES/$INSTALL_KBYTES/g" $PACKAGEDIR/PackageInfo
 
 # create Bom
 echo "Creating Bom..."
@@ -43,12 +52,12 @@ RET=$?
 
 if [ "$RET" -ne 0 ]
 then
-  echo "mkbom -u 0 -g 0 $APPDIR PACKAGEDIR/Bom returned $RET"
+  echo "mkbom -u 0 -g 0 $APPDIR $PACKAGEDIR/Bom returned $RET"
   exit 1
 fi
 
 echo "Creating Payload..."
-find $APPDIR | cpio -o --format odc --owner 0:80 | gzip -c -9 > $PACKAGEDIR/Payload
+find $APPDIR | cpio -o --format odc --owner 0:80 | gzip -c -9 > $PKGDIR/PACKAGEDIR/Payload
 
 echo "Creating PKG..."
-xar --compression none -cf $PKGFILE $PKGDIR/*
+xar --compression none -cf $PKGFILE PKGDIR/*
